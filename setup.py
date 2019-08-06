4982a3bbc9009e5c2e409e5a2a572aec8a324e07
yassin-taskin <taskin@collide.info>
2019-08-06 16:17:44 +0200

    Added setup.py and License.md

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='esa_ttn',
+    version='0.1',
+    packages=['esa_ttn'],
+    url='',
+    license='MIT',
+    author='yassin',
+    author_email='taskin@collide.info',
+    description='Create a network representation of text base on Explicit Semantic Analysis'
+)
